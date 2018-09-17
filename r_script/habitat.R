64e275539d9611d1aa49e4db3a721dc5376eefc1
Jesse Borden <jborden@ufl.edu>
2018-09-17 10:49:47 -0400

    add columns in original excel for avg canopy details. Create HabSummary csv for habitat summary details

--- r_script/habitat.R
+++ r_script/habitat.R
@@ -2,4 +2,10 @@
 
 #Data
 habitat <- read.csv("raw_data/habitat.csv", header = TRUE)
-habitat <- habitat[,-1]
+
+
+HabSummary <- habitat %>%
+  select(Tree_ID, TotalAvgCan = total_avg_can_cov, CanMinus10 = can_minus_10, CanMinus5 = can_minus_5,
+         Can0 = can_0, CanPlus5 = can_plus_5, CanPlus10 = can_plus_10,
+         AvgHerpCover = avg_herb_cover..., AvgLeafLayer = avg_leaf_layer,
+         StemLess8cm = stem_less_8cm, StemMore8cm = stem_more_8cm)
