9a416b77856309b806ac6fb0b20e009118b3c750
Jesse Borden <jborden@ufl.edu>
2018-09-18 13:38:17 -0400

    write code into same dplyr section to change canopy readings from % sky to % canopy

--- r_script/habitat.R
+++ r_script/habitat.R
@@ -8,20 +8,21 @@ HabSummary <- habitat %>% #select summary columns
   select(Tree_ID, TotalAvgCan = total_avg_can_cov, CanMinus10 = can_minus_10, CanMinus5 = can_minus_5,
          Can0 = can_0, CanPlus5 = can_plus_5, CanPlus10 = can_plus_10,
          AvgHerbCover = avg_herb_cover..., AvgLeafLayer = avg_leaf_layer,
-         StemLess8cm = stem_less_8cm, StemMore8cm = stem_more_8cm)
+         StemLess8cm = stem_less_8cm, StemMore8cm = stem_more_8cm)%>%
+  mutate(TotalAvgCan = (100 - TotalAvgCan))%>%
+  mutate(CanMinus10 = (100 - CanMinus10))%>%
+  mutate(CanMinus5 = (100 - CanMinus5))%>%
+  mutate(Can0 = (100 - Can0))%>%
+  mutate(CanPlus5 = (100 - CanPlus5))%>%
+  mutate(CanPlus10 = (100 - CanPlus10))
+#mutate adds column in which we 
+#transform these columns from densiometer readings (which gives
+# measure of sky, not canopy), to now be % canopy
 
 
 HabSummary <- HabSummary %>%
   distinct(Tree_ID, .keep_all = TRUE) #remove duplicates
 
-#transform these columns from densiometer readings (which gives
-# measure of sky, not canopy), to now be % canopy
-HabSummary$TotalAvgCan = 100 - HabSummary$TotalAvgCan
-HabSummary$CanMinus10 = 100 - HabSummary$CanMinus10
-HabSummary$CanMinus5 = 100 - HabSummary$CanMinus5
-HabSummary$Can0 = 100 - HabSummary$Can0
-HabSummary$CanPlus5 = 100 - HabSummary$CanPlus5
-HabSummary$CanPlus10 = 100 - HabSummary$CanPlus10
 
 
 write_csv(HabSummary, "clean_data/HabSummary.csv") #write the new dataset as a csv
