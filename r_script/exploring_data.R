0df622bf2af55dd6420014efa0f2776a28c6e2ab
GillesGaelBERNARD <gillesbernardgilles@gmail.com>
2019-11-19 22:25:21 +0100

    adding some vertical distribution graphs to explore data

--- r_script/exploring_data.R
+++ r_script/exploring_data.R
@@ -167,3 +167,112 @@ ggplot(HerpHabM, aes(edge_category_m, fill = binomial))+
 ggplot(HerpHabCY, aes(edge_category_m, fill = binomial))+
   geom_histogram()+
   ggtitle("Cynometera Forest")
+
+
+
+
+
+##### VERTICAL DISTRIBUTIONS GRAHPS
+
+#vertical height distribution by edge category by forest type, same sampling effort
+
+ggplot(data = filter(HerpHabMat, survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(forest_type~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "Vertical distribution by edge category by forest type (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+#vertical height distribution by forest type, same sampling effort
+
+ggplot(data = filter(HerpHabMat, survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(.~forest_type, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "Vertical distribution by forest type (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+#vertical height distribution by edge category, same sampling effort
+
+ggplot(data = filter(HerpHabMat, survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(.~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "Vertical distribution by edge category (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+
+#vertical height distribution by edge category by forest type, Climber surveys only
+
+ggplot(data = filter(HerpHabMat, survey_type == "C")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(forest_type~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "Vertical distribution by edge category be forest type (C surveys only)", y = "Relative density", x = "Vertical height (m)")
+
+
+
+# HEPL
+ggplot(data = filter(HerpHabMat, species_code == "HEPL", survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(forest_type~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "HEPL vertical distribution by edge category by forest type (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+
+# HEMA
+ggplot(data = filter(HerpHabMat, species_code == "HEMA", survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(forest_type~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "HEMA vertical distribution by edge category by forest type (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+# LYMO
+ggplot(data = filter(HerpHabMat, species_code == "LYMO", survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(forest_type~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "LYMO vertical distribution by edge category by forest type (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+# CHDI
+ggplot(data = filter(HerpHabMat, species_code == "CHDI", survey_type == "C" | survey_type == "G", extra_ground != "Y", category != "matrix")) + geom_density(mapping = aes(x= height_found_m), alpha = 1, linetype = 1) + facet_grid(forest_type~edge_category_m, scales = "free_x") +
+  theme(axis.title.y = element_text(size = rel(1.5),face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.title.x = element_text(size = rel(1.5),face = "bold", margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.text.x = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        axis.text.y = element_text(color = "black", face = "bold",  size = rel(1.3)),
+        strip.text.x = element_text(colour = "black", face = "italic"),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_rect(colour = "black", fill = "white"), strip.background = element_rect(colour = "black", fill = "white")) +  coord_flip(xlim = c(0,20))+  guides(fill = guide_legend(override.aes = list(alpha = 1))) + theme(axis.text.x=element_blank(), axis.line=element_blank(), axis.ticks=element_blank()) + labs(title = "CHDI vertical distribution by edge category by forest type (G and C surveys, no matrix)", y = "Relative density", x = "Vertical height (m)")
+
+
+
+
+
+
+
+
+
