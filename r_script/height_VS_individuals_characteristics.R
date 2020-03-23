03e5372aa9b0d0804c8f62473af632dd33d212be
GillesGaelBERNARD <gillesbernardgilles@gmail.com>
2020-03-23 10:50:28 +0100

    combined graph

--- r_script/height_VS_individuals_characteristics.R
+++ r_script/height_VS_individuals_characteristics.R
@@ -91,7 +91,7 @@ lizards_Id_good_sample_species <- lizards_Id_good_sample_species %>% filter(spec
 
 
 # CHDI
-a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "CHDI")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height_VS_SVL, CHDI", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
+a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "CHDI")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height VS SVL, CHDI", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
 data_frame <- filter(lizards_Id_good_sample_species, species_code == "CHDI")
 lm_eqn <- function(data_frame){
   m <- lm(height_found_m ~ SVL_cm, data_frame);
@@ -103,7 +103,7 @@ a
 graph1 <- a
 
 # HEMA
-a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "HEMA")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height_VS_SVL, HEMA", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
+a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "HEMA")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height VS SVL, HEMA", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
 data_frame <- filter(lizards_Id_good_sample_species, species_code == "HEMA")
 lm_eqn <- function(data_frame){
   m <- lm(height_found_m ~ SVL_cm, data_frame);
@@ -115,7 +115,7 @@ a
 graph2 <- a
 
 # HEMI
-a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "HEMI")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height_VS_SVL, HEMI", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
+a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "HEMI")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height VS SVL, HEMI", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
 data_frame <- filter(lizards_Id_good_sample_species, species_code == "HEMI")
 lm_eqn <- function(data_frame){
   m <- lm(height_found_m ~ SVL_cm, data_frame);
@@ -127,7 +127,7 @@ a
 graph3 <- a
 
 # HEPL
-a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "HEPL")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height_VS_SVL, HEPL", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
+a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "HEPL")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height VS SVL, HEPL", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
 data_frame <- filter(lizards_Id_good_sample_species, species_code == "HEPL")
 lm_eqn <- function(data_frame){
   m <- lm(height_found_m ~ SVL_cm, data_frame);
@@ -139,7 +139,7 @@ a
 graph4 <- a
 
 # LYMO
-a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "LYMO")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height_VS_SVL, LYMO", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
+a <- ggplot(data = filter(lizards_Id_good_sample_species, species_code == "LYMO")) + geom_point(mapping = aes(x = SVL_cm, y = height_found_m))  + geom_smooth(mapping = aes(x = SVL_cm, y = height_found_m), method = "lm") + labs(title = "Height VS SVL, LYMO", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
 data_frame <- filter(lizards_Id_good_sample_species, species_code == "LYMO")
 lm_eqn <- function(data_frame){
   m <- lm(height_found_m ~ SVL_cm, data_frame);
@@ -151,15 +151,26 @@ a
 graph5 <- a
 
 
+# all animals < 20 cm
+a <- ggplot(data = lizards, mapping = aes(x = SVL_cm, y = height_found_m)) + geom_point() + geom_smooth(method = "lm") + labs(title = "Height VS SVL, lizards/geckos", x = "SVL (cm)", y = "Height (m)") + theme_bw(base_size = 23)
+data_frame <- lizards
+lm_eqn <- function(data_frame){
+  m <- lm(height_found_m ~ SVL_cm, data_frame);
+  eq <- substitute(italic(r)~"="~rvalue*","~italic(p)~"="~pvalue, list(rvalue = sprintf("%.2f",sign(coef(m)[2])*sqrt(summary(m)$r.squared)), pvalue = format(summary(m)$coefficients[2,4], digits = 2)))
+  as.character(as.expression(eq));                 
+}
+a <- a + geom_text(x = 10, y = 12.5, label = lm_eqn(data_frame), parse = TRUE, size = 8)
+a
+graph0 <- a
+
+
 
 png("figures/height_VS_SLV.png", width = 1000, height = 1200)
-plot_grid(graph1, graph2, graph3, graph4, graph5, ncol=2, labels=c("A", "B", "C", "D", "E"), label_size = 23)
+plot_grid(graph0, graph1, graph2, graph3, graph4, graph5, ncol=2, labels=c("A", "B", "C", "D", "E", "F"), label_size = 23)
 dev.off()
 
 
 
-
-
-
+      
 
 
